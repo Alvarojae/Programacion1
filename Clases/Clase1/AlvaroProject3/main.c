68c4da6055daa12e31d6b691a03cdcfbfea7d479
alvarojae <alvaroelena97@gmail.com>
2019-03-18 12:03:15 -0300

    alvaro

---
+++ Clases/Clase1/AlvaroProject3/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    int numero = 10 ;
+    int numero2 = 34 ;
+    char letra = 'a';
+    float altura = 23.5;
+
+    numero2 = altura;
+
+    printf("la altura vale %d", numero2 );
+    return 0;
+}
+
+
+//%.2f (muestra solo dos decimales despues)
+
+//%.d f (muestra el numero redondeado)
+
+
+
+
+
+
+/*
+int main()
+{
+    int numero = 10 ;
+    int numero2 = 34 ;
+    char letra = 'a';
+
+    printf("la variable letra vale %c", letra );
+    return 0;
+}
+
+//printf("la variable numero vale %d y la segunda variable vale %d", numero, numero2);
+
+int main()
+{
+    int numero = 10 ;
+    int numero2 = 34 ;
+
+    printf("la variable numero vale %d", numero + numero2 );
+    return 0;
+}
+*/
