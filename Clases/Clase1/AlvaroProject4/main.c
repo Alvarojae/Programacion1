67fc266d9add822e7b43441e6cb53260a60058a1
alvarojae <alvaroelena97@gmail.com>
2019-03-18 12:16:48 -0300

    alvaro

---
+++ Clases/Clase1/AlvaroProject4/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numero;
+    printf("ingrese un numero: ");
+    scanf("%d", &numero);
+    printf("usted ingreso el numero: %d", numero);
+    return 0;
+}
+
+//scanf("%d", &numero);(!!solo en scanf())
