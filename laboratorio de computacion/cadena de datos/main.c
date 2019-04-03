dd6047bb2d542e8d3d1aec2f3ea4c99bc602ebd8
alvarojae <alvaroelena97@gmail.com>
2019-04-03 12:09:46 -0300

    alvaro

---
+++ laboratorio de computacion/cadena de datos/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char cadena[50];
+    //scanf("%s",cadena);
+    gets(cadena);
+    fflush(stdin);
+    printf("%s",cadena);
+
+    return 0;
+}
+
+
+
+
+/** \\ cadenas de caracters
+ *      string
+ *
+ *  char cadena[50];
+ *  scanf("%s",cadena);
+ *  fflush(stdin);
+ *  printf("%s",cadena);
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+
+
