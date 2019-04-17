72b153efaa1504dd253e62fd0d036a08dcc558fd
alvarojae <alvaroelena97@gmail.com>
2019-04-17 10:08:18 -0300

    alvaro

---
+++ laboratorio de computacion/estructuras2/empleado.c
@@ -0,0 +1,143 @@
+#include "empleado.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <conio.h>
+void inicializarEmpleados(eEmpleado vec[], int tam)
+{
+
+    for(int i=0; i < tam; i++)
+    {
+        vec[i].ocupado = 0;
+    }
+}
+
+int menu()
+{
+    int opcion;
+
+    system("cls");
+    printf("  *** ABM Empleados ***\n\n");
+    printf("1- Alta Empleado\n");
+    printf("2- Baja Empleado\n");
+    printf("3- Modificacion Empleado\n");
+    printf("4- Ordenar Empleados\n");
+    printf("5- Listar Empleados\n");
+    printf("6- Salir\n\n");
+    printf("Ingrese opcion: ");
+    scanf("%d", &opcion);
+
+    return opcion;
+}
+
+void mostrarEmpleado(eEmpleado emp){
+
+    printf("   %d  %10s  %c  %.2f  %d/%d/%d\n", emp.legajo, emp.nombre, emp.sexo, emp.sueldo, emp.fechaNac.dia,emp.fechaNac.mes,emp.fechaNac.anio);
+
+}
+
+void mostrarEmpleados(eEmpleado vec[], int tam){
+    int contador = 0;
+
+    printf(" Legajo  Nombre     Sexo      Sueldo   Nacimiento\n\n");
+    for(int i=0; i < tam; i++){
+        if(vec[i].ocupado == 1){
+            mostrarEmpleado(vec[i]);
+            contador++;
+        }
+    }
+
+    if( contador == 0){
+        printf("\nNo hay empleados que mostrar\n");
+    }
+}
+
+int buscarLibre(eEmpleado vec[], int tam){
+
+    int indice = -1;
+
+     for(int i=0; i < tam; i++)
+    {
+        if(vec[i].ocupado == 0){
+            indice = i;
+            break;
+        }
+    }
+
+    return indice;
+}
+
+int buscarEmpleado(eEmpleado vec[], int tam, int legajo){
+
+    int indice = -1;
+
+     for(int i=0; i < tam; i++)
+    {
+        if(vec[i].ocupado == 1 && vec[i].legajo == legajo){
+            indice = i;
+            break;
+        }
+    }
+
+    return indice;
+}
+
+void altaEmpleado(eEmpleado vec[], int tam){
+
+int indice;
+int legajo;
+int esta;
+
+
+indice = buscarLibre(vec, tam);
+
+if( indice == -1){
+
+    printf("\nNo hay lugar en el sistema\n");
+}
+else{
+     printf("Ingrese legajo: ");
+     scanf("%d", &legajo);
+
+     esta = buscarEmpleado(vec, tam, legajo);
+
+     if( esta != -1){
+        printf("Existe un empleado de legajo %d en el sistema\n", legajo);
+        mostrarEmpleado(vec[esta]);
+     }
+     else{
+        vec[indice].legajo = legajo;
+
+        printf("Ingrese nombre: ");
+        fflush(stdin);
+        gets(vec[indice].nombre);
+
+        printf("Ingrese sexo: ");
+        fflush(stdin);
+        scanf("%c", &vec[indice].sexo );
+
+        printf("Ingrese sueldo: ");
+        scanf("%f", &vec[indice].sueldo );
+
+        printf("Ingrese fecha de nacimiento: \n");
+
+         printf("dia : ");
+        scanf("%int", &vec[indice].fechaNac.dia );
+
+         printf("mes : ");
+        scanf("%int", &vec[indice].fechaNac.mes );
+
+         printf("anio: ");
+        scanf("%int", &vec[indice].fechaNac.anio );
+
+
+        vec[indice].ocupado = 1;
+
+        printf("Alta empleado exitosa!!!\n\n");
+
+     }
+
+}
+}
+
+
---
+++ laboratorio de computacion/estructuras2/empleado.h
@@ -0,0 +1,25 @@
+typedef struct
+{
+    int dia,mes,anio;
+} eFecha;
+
+
+typedef struct
+{
+
+    int legajo;
+    char nombre[20];
+    char sexo;
+    float sueldo;
+    int ocupado;
+    eFecha fechaNac;
+} eEmpleado;
+
+void inicializarEmpleados(eEmpleado vec[], int tam);
+void mostrarEmpleado(eEmpleado emp);
+void mostrarEmpleados(eEmpleado vec[], int tam);
+int buscarLibre(eEmpleado vec[], int tam);
+int buscarEmpleado(eEmpleado vec[], int tam, int legajo);
+void altaEmpleado(eEmpleado vec[], int tam);
+
+
